0fe3c3a61bffb4a1644d61ec7b5a651919823d58
xiaoma <xiaoma@ubuntu-VirtualBox.(none)>
2012-07-12 18:53:25 -0700

    added put and post

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ app.set('view engine', 'ejs');
 app.use(express.bodyParser());
 
 
-var wines = [{"name": "Midland Vinyards Red",
-							"grapes": "red",
-							"country": "Ruritania",
-							"region": "Midland slopes",
-							"year": "1834",
-							"description": "very smooth"}];
-
-
 app.get('/api/wines', function(req, response) {
 	rwines = redisClient.lrange('wines',0,-1,function(err, wines){
 		wines = wines.map(JSON.parse);
@@ -26,16 +18,29 @@ app.get('/api/wines', function(req, response) {
 
 app.get('/api/wines/:wine', function(req, response) {
 	var id = req.params.wine;
-	wine = redisClient.lrange('wines', id, id, function(err, wine){
+	wine = redisClient.lindex('wines', id, function(err, wine){
 		console.log(wine);
-		response.render('wine.ejs', {id: id, wine: JSON.parse(wine)}) 
+		response.render('wine.ejs', {id: id, wine: JSON.parse(wine)});
 	});
-	//response.render('wine.ejs', {id: id, wine: wines[id]}) 
 });
 
 app.post('/api/wines', function(req, response) {
-	console.log(req.body);
-  redisClient.rpush('wines', JSON.stringify(req.body), function(err, wine){
+	wine = req.body;
+	console.log(wine);
+  	redisClient.rpush('wines', JSON.stringify(wine), function(err, wine){
+  		redisClient.llen('wines', function(err, length){
+  			id = length-1;
+  			response.render('wine.ejs', {id: 1, wine: wine});
+  		});
+	});
+});
+
+app.put('/api/wines/:wine', function(req, response) {
+	var id = req.params.wine;
+	wine = req.body;
+	console.log(wine);
+	redisClient.lset('wines', id, JSON.stringify(wine), function(err){
+		response.render('wine.ejs', {id: id, wine: wine});
 	});
 });
 
